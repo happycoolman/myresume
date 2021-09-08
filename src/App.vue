4b181bc9a7c5a1dd30cb16477409c8b5e3bca11a
kaixingg <1536863186@qq.com>
2021-09-08 14:58:01 +0800

    deploy

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,13 @@
     <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'app',
+    mounted() {
+      const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
+    }
+  }
+</script>
